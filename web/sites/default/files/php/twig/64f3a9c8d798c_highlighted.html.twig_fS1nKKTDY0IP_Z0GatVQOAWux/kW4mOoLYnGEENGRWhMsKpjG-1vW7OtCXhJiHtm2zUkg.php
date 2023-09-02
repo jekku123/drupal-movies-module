39d70900c80a2f6da2c09c12fbefc7c1784f7b5b
jekku123 <jesse.manninen@gmail.com>
2023-09-03 00:35:49 +0300

    test

---
+++ web/sites/default/files/php/twig/64f3a9c8d798c_highlighted.html.twig_fS1nKKTDY0IP_Z0GatVQOAWux/kW4mOoLYnGEENGRWhMsKpjG-1vW7OtCXhJiHtm2zUkg.php
@@ -0,0 +1,100 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @mahi/parts/highlighted.html.twig */
+class __TwigTemplate_801e2c0ff6dc06ba5e4ac315091d8f35 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        if (twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "highlighted", [], "any", false, false, true, 1)) {
+            // line 2
+            echo "<div class=\"highlighted\">
+   <div class=\"container\">
+      ";
+            // line 4
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "highlighted", [], "any", false, false, true, 4), 4, $this->source), "html", null, true);
+            echo "
+   </div>
+</div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "@mahi/parts/highlighted.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 4,  41 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "@mahi/parts/highlighted.html.twig", "/app/web/themes/contrib/mahi/templates/parts/highlighted.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 1);
+        static $filters = array("escape" => 4);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
